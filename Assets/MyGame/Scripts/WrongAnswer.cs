a0c486e688046be045d76a7ca0c5204b671b393e
Jps2310 <janphillip.fps@gmail.com>
2021-04-13 09:37:31 +0200

    Second Question working

---
+++ Assets/MyGame/Scripts/WrongAnswer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WrongAnswer : MonoBehaviour
+{
+
+    public Button a;
+ 
+
+
+
+    public void FalseAnswer()
+    {
+        
+           
+            a.image.color = new Color32(255, 0, 0, 255);
+            
+            
+
+
+    }
+
+}
+
